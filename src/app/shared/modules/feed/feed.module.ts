5173a728878febb152a3b7603b23acb2444da7cf
i.plotnikov <ploBcvxw3>
2023-01-06 09:40:17 +0400

    feat: Добавил эффект для получения постов

--- src/app/shared/modules/feed/feed.module.ts
+++ src/app/shared/modules/feed/feed.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { EffectsModule } from '@ngrx/effects'
 
 import { FeedComponent } from './components/feed.component'
+import { GetFeedEffect } from './store/effects/getFeed.effect'
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, EffectsModule.forFeature([GetFeedEffect])],
   declarations: [FeedComponent],
   exports: [FeedComponent]
 })
