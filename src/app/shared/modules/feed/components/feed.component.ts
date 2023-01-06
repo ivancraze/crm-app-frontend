3ba4ffac46c7f962a78a158253d8420754ab1bad
i.plotnikov <ploBcvxw3>
2023-01-06 14:48:24 +0400

    feat: Доработал компонент фида

--- src/app/shared/modules/feed/components/feed.component.ts
+++ src/app/shared/modules/feed/components/feed.component.ts
@@ -1,10 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { Component, Input, OnDestroy, OnInit } from '@angular/core'
+import { ActivatedRoute, Params, Router } from '@angular/router'
 import { select, Store } from '@ngrx/store'
-import { getFeedAction } from '../store/actions/getFeed.action'
-import { Observable } from 'rxjs'
+import { Observable, Subscription } from 'rxjs'
+import queryString from 'query-string'
 
 import { GetFeedResponseInterface } from '../types/getFeedResponse.interface'
+import { getFeedAction } from '../store/actions/getFeed.action'
 import { errorSelector, feedSelector, isLoadingSelector } from '../store/selectors'
+import { environment } from '../../../../../environments/environment'
 
 @Component({
   selector: 'mc-feed',
@@ -12,27 +15,50 @@ import { errorSelector, feedSelector, isLoadingSelector } from '../store/selecto
   styleUrls: ['./feed.component.scss']
 })
 
-export class FeedComponent implements OnInit {
-  constructor(private store: Store) {}
+export class FeedComponent implements OnInit, OnDestroy {
+  constructor(private store: Store, private router: Router, private route: ActivatedRoute) {}
 
   @Input('apiUrl') apiUrlProps: string
-
   isLoading$: Observable<boolean>
   error$: Observable<string | null>
   feed$: Observable<GetFeedResponseInterface | null>
+  limit = environment.limit
+  baseUrl: string
+  queryParamsSubscription: Subscription
+  currentPage: number
 
   ngOnInit(): void {
     this.initializeValues()
-    this.fetchData()
+    this.initializeListeners()
+  }
+
+  ngOnDestroy(): void {
+    this.queryParamsSubscription.unsubscribe()
   }
 
   initializeValues(): void {
     this.isLoading$ = this.store.pipe(select(isLoadingSelector))
     this.error$ = this.store.pipe(select(errorSelector))
     this.feed$ = this.store.pipe(select(feedSelector))
+    this.baseUrl = this.router.url.split('?')[0]
   }
 
-  fetchData(): void {
-    this.store.dispatch(getFeedAction({url: this.apiUrlProps}))
+  initializeListeners(): void {
+    this.queryParamsSubscription = this.route.queryParams.subscribe((params: Params) => {
+      this.currentPage = Number(params.page || '1')
+      this.fetchFeed()
+    })
+  }
+
+  fetchFeed(): void {
+    const offset = this.currentPage * this.limit - this.limit
+    const parsedUrl = queryString.parseUrl(this.apiUrlProps)
+    const stingifiedParams = queryString.stringify({
+      limit: this.limit,
+      offset,
+      ...parsedUrl.query
+    })
+    const apiUrlWithParams = `${parsedUrl.url}?${stingifiedParams}`
+    this.store.dispatch(getFeedAction({url: apiUrlWithParams}))
   }
 }
